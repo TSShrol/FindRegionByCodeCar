c39fcc9712a4c3094f1c1f897028d5126e6163bf
TSShrol <shrolts@gmail.com>
2023-04-12 18:17:50 +0300

    Add project files.

---
+++ CarCodeClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace CarCodeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+
+            NetworkStream stream = null;
+           
+            StreamReader reader = null;
+            StreamWriter writer = null;
+
+            try
+            {
+                //tcpClient.Connect("127.0.0.1",8008);
+                tcpClient.Connect("10.7.150.203", 8008);
+                stream = tcpClient.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+
+                while (true)
+                {
+                    Console.Write("Input message>> ");
+                    var message = Console.ReadLine();
+                    writer.WriteLine(message);
+                    writer.Flush();
+
+                    var response_msg=reader.ReadLine();
+                    Console.WriteLine("You received the following message : " + response_msg);
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null) stream.Close();
+                tcpClient.Close();
+                reader.Close();
+                writer.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CarCodeServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace CerCodeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8008);
+            NetworkStream stream = null;
+            try
+            {
+                tcpListener.Start();
+                Console.WriteLine( "Server waiting...");
+                
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                  
+                    Task.Run(() =>
+                    {
+                        Console.WriteLine($"Client connected... {tcpClient.Client.RemoteEndPoint}");
+                        stream = tcpClient.GetStream();
+                        StreamReader reader = new StreamReader(stream);
+                        StreamWriter writer = new StreamWriter(stream);
+                     
+                        while (tcpClient.Connected)
+                        { 
+                            //request code of car
+                            string message = reader.ReadLine();
+
+                            Console.WriteLine("You received the following message : " + message);
+                            Console.WriteLine($"{DateTime.Now.ToLongTimeString()} :: {message} ");
+
+                            //response server for client: region                          
+                            string response_msg = "Region";
+                            writer.WriteLine(response_msg);
+                            writer.Flush();
+                        }
+
+                        reader.Close();
+                        writer.Close();
+                    });
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                stream.Close();
+                tcpListener.Stop();
+               
+            }
+        } 
+    }
+}
